473e9bb0efe7cb255b306f0a12ca27a439cc1c6b
Sujal Naroia <sujalnaroia0099@gmail.com>
2023-03-05 22:40:37 -0600

    Added custom classes to sketch and made ship move

--- AsteroidGame/sketch.js
+++ AsteroidGame/sketch.js
@@ -1,8 +1,51 @@
+let ship;
+let shipRotation = 0;
+let shipRotator = 0;
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(400, 400);
+
+  angleMode(DEGREES);
+  let startingPosition = createVector(width / 2, height / 2);
+  let startingVelocity = createVector(0, 0);
+  let spriteSize = 5;
+  let sprite = 0;
+  let health = 5;
+  ship = new Character(
+    startingPosition,
+    startingVelocity,
+    spriteSize,
+    sprite,
+    health
+  );
 }
 
-function draw() 
-{
-    
+function draw() {
+  background(0);
+
+  shipRotation += shipRotator;
+
+  push();
+
+  ship.rotateShip(shipRotation);
+  ship.update(mouseIsPressed);
+  ship.display();
+
+  pop();
+}
+
+function keyPressed() {
+  switch (key) {
+    case "a":
+      shipRotator = -5;
+      break;
+
+    case "d":
+      shipRotator = 5;
+      break;
+  }
+}
+
+function keyReleased() {
+  shipRotator = 0;
 }
