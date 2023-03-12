e562e7834f4fc459379a67bf0bbe9b5d492fba37
Sujal Naroia <sujalnaroia0099@gmail.com>
2023-03-12 00:45:30 -0600

    Lasers have a lifespan

--- AsteroidGame/laser.js
+++ AsteroidGame/laser.js
@@ -6,10 +6,18 @@ class Laser extends Actor {
     super(startingPosition, startingVelocity, size);
 
     this.isPlayerLaser = isPlayerLaser;
+    this.health = 180;
+  }
+
+  update() {
+    this.health--;
+    super.update();
   }
 
   display() {
     stroke(255);
+
+    if (!this.isPlayerLaser) stroke(255, 0, 0);
     strokeWeight(4);
     point(this.position.x, this.position.y);
   }
